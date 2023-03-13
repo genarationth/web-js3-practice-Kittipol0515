cb071be80c4f20b11798001914e5df4d170b0ef6
kittipol <kittipol.surapinchai@gmail.com>
2023-03-13 16:36:10 +0700

    JS work

---
+++ script.js
@@ -0,0 +1,22 @@
+function add(a, b) {
+    return a + b;
+  }
+// console.log (add(5,3));
+let result = add(5,3);  
+console.log (result);
+
+function subtract(a,b) {
+    return a - b;
+}
+console.log (subtract(10,5));
+
+function multiply(a,b) {
+    return a * b;
+}
+console.log (multiply(10,5));
+
+function divide(a,b) {
+    return a / b ;
+}
+let answer = divide(100,10);
+console.log (answer);
